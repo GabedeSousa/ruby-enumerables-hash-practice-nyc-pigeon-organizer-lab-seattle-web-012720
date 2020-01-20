4a533b1de2c653e9318091ee9623902a114a08eb
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-20 20:05:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,13 @@ piegeon_list = {}
 data.each do |key,values|
   values.each do |value,names|
     names.each do |name|
-      piegeon_list[name] || = {}
-      piegeon_list[name][key] || = {}
+      piegeon_list[name] ||= {}
+      piegeon_list[name][key] ||= {}
       piegeon_list[name][key] << value.to_s 
     end
   end
 end
 piegeon_list
 end
+
 
