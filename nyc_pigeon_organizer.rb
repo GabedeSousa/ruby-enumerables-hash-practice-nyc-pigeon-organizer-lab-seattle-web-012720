277dc34a8f237dc4f025117e2c17b38b7e993b2a
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-20 20:08:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,55 @@
 def nyc_pigeon_organizer(data)
-pigeon_list = {}
-data.each do |key,values|
-  values.each do |value,names|
-    names.each do |name|
-      pigeon_list[name] ||= {}
-      pigeon_list[name][key] ||= {}
-      pigeon_list[name][key] << value.to_s 
-    end
-  end
+newHash = Hash.new
+
+  data.each_key {|q| #color/gender/lives
+
+    data[q].each_key {|w| #subcategories of above(for example purple/grey/white)
+
+
+      w_string = w.to_s 
+
+      data[q][w].map {|e| #pigeon names
+
+        if (newHash[e])
+
+
+          if (newHash[e][q])
+            #puts "\n\n\n%%%%%%%%%%%%%%%\n\nMade it to if newHash[][]"
+
+            if (newHash[e][q].include?(w_string))
+            else
+              #puts "\n\n\n\nmade it here to #{w_string}\n\n\n"
+              newHash[e][q].push(w_string)
+            end
+          else
+            newHash[e][q] = []
+            newHash[e][q].push(w_string)
+
+
+          end
+
+
+        else
+          newHash[e] = Hash.new
+          newHash[e][q] = []
+          newHash[e][q].push(w_string)
+        end
+
+
+
+
+      }
+
+    }
+
+
+
+  }
+
+  newHash
+
 end
-pigeon_list
-end
+
 
 
 
